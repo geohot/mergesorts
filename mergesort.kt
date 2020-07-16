e24ff29dda762b3f5f373e6a42ff3edfa8b82467
Anantha Raju C <arcswdev@gmail.com>
2020-07-16 19:28:05 +0530

    add kotlin

---
+++ mergesort.kt
@@ -0,0 +1,44 @@
+fun mergeSort(list: List<Int>): List<Int> {
+    if (list.size <= 1) {
+        return list
+    }
+
+    val middle = list.size / 2
+    var left = list.subList(0,middle);
+    var right = list.subList(middle,list.size);
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+fun merge(left: List<Int>, right: List<Int>): List<Int>  {
+    var indexLeft = 0
+    var indexRight = 0
+    var newList : MutableList<Int> = mutableListOf()
+
+    while (indexLeft < left.count() && indexRight < right.count()) {
+        if (left[indexLeft] <= right[indexRight]) {
+            newList.add(left[indexLeft])
+            indexLeft++
+        } else {
+            newList.add(right[indexRight])
+            indexRight++
+        }
+    }
+
+    while (indexLeft < left.size) {
+        newList.add(left[indexLeft])
+        indexLeft++
+    }
+
+    while (indexRight < right.size) {
+        newList.add(right[indexRight])
+        indexRight++
+    }
+    return newList;
+}
+
+fun main(args: Array<String>) {
+    val numbers = mutableListOf(38,27,43,3,9,82,10)
+    val sortedList = mergeSort(numbers)
+    println("Unsorted: $numbers")
+    println("Sorted: $sortedList")
+}
