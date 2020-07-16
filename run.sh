7b168d4258d30180297bd67b4c633a56322f4d86
exozero7 <foti.kerkeshi@hotmail.com>
2020-07-16 08:31:00 +0200

    update

--- run.sh
+++ run.sh
@@ -4,4 +4,3 @@ python3 mergesort.py
 rm -f mergesort && ghc mergesort.hs && ./mergesort
 rm -f mergesort && rustc mergesort.rs && ./mergesort
 rhino mergesort.js
-rm -f MergeSort.class && javac mergesort.java && java MergeSort 
