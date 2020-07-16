10a7fcaf5b0dc2098d9ff2ea19633a416e7ae9f6
TrAyZeN <1810leo@gmail.com>
2020-07-16 12:30:19 +0200

    Add ocaml mergesort

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ rm -f mergesort && rustc mergesort.rs && ./mergesort
 rm -f Mergesort && javac Mergesort.java && java Mergesort
 rhino mergesort.js
 php mergesort.php
+rm -f mergesort && ocamlc mergesort.ml -o mergesort && ./mergesort
