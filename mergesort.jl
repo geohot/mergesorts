e42c96059cfb95c9fb34649c28405d90b6a97fbc
abir <abirchandrabhushan@gmail.com>
2020-07-16 13:24:44 +0530

    add exec to run.sh + now using the same list

--- mergesort.jl
+++ mergesort.jl
@@ -1,27 +1,44 @@
-function mergesort(arr::Vector)
-    if length(arr) ≤ 1 return arr end
-    mid = length(arr) ÷ 2
-    lpart = mergesort(arr[1:mid])
-    rpart = mergesort(arr[mid+1:end])
-    rst = similar(arr)
-    i = ri = li = 1
-    @inbounds while li ≤ length(lpart) && ri ≤ length(rpart)
-        if lpart[li] ≤ rpart[ri]
-            rst[i] = lpart[li]
-            li += 1
-        else
-            rst[i] = rpart[ri]
-            ri += 1
+function mergesort(arr)
+    function merge(m1, m2)
+        j = 1
+        k = 1
+        result = Int64[]
+        while true
+            if m1[j] > m2[k]
+                append!(result, [m2[k]])
+                k += 1
+            else
+                append!(result, [m1[j]])
+                j += 1
+            end
+            if j == length(m1) + 1
+                append!(result, m2[k:end])
+                break
+            end
+            if k == length(m2) + 1
+                append!(result, m1[j:end])
+                break
+            end
         end
-        i += 1
+        return result
     end
-    if li ≤ length(lpart)
-        copy!(rst, i, lpart, li)
+
+    n = length(arr)
+    if n < 2
+        return arr
+    elseif n == 2
+        if arr[1] > arr[2]
+            temp = arr[1]
+            arr[1] = arr[2]
+            arr[2] = temp
+        end
+        return arr
     else
-        copy!(rst, i, rpart, ri)
+        lhs = mergesort(arr[1 : div(n, 2)])
+        rhs = mergesort(arr[div(n, 2) + 1 : end])
+        return merge(lhs, rhs)
     end
-    return rst
 end
  
-v = rand(-10:10, 10)
+v = [5,9,1,3,4,6,6,3,2]
 println("# unordered: $v\n -> ordered: ", mergesort(v))
