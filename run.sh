19677ac7e3e8f2e031a32c321a77544a616baa4a
PulakIIIT <jimrohn28@gmail.com>
2020-07-16 12:02:11 +0530

    c++, fixed list and run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ rm -f mergesort && rustc mergesort.rs && ./mergesort
 rm -f Mergesort && javac Mergesort.java && java Mergesort
 rhino mergesort.js
 php mergesort.php
+rm -f mergesort && g++ -std=c++17 mergesort.cpp && ./a.out
