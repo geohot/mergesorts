a37bc9cebafad390a1a1c1ea0eeac3cd1f1d96fd
SebYLim <sebylim01@gmail.com>
2020-07-16 13:45:52 -0400

    Updated Java file to follow similar format and changed naming scheme to match

--- Mergesort.java
+++ Mergesort.java
@@ -1,45 +1,41 @@
 import java.util.Arrays;
 
-public class Mergesort {
+public class mergesort {
     
-    public static int[] sort(int arr[]) {
-        int n = arr.length;
+    public static int[] my_mergesort(int a[]) {
+        int n = a.length;
         if (n == 1) {
-            return arr;
+            return a;
         }
 
-        int[] left = Arrays.copyOfRange(arr, 0, n / 2);
-        int[] right = Arrays.copyOfRange(arr, n / 2, n);
-        left = sort(left);
-        right = sort(right);
-        return merge(left, right);
-    }
-
-    public static int[] merge(int[] left, int[] right) {
+        int[] left = Arrays.copyOfRange(a, 0, n / 2);
+        int[] right = Arrays.copyOfRange(a, n / 2, n);
+        left = my_mergesort(left);
+        right = my_mergesort(right); 
+	
         int[] arr = new int[left.length + right.length];
         int l = 0, r = 0, i = 0;
-        
-        while (l < left.length && r < right.length) {
-            if (left[l] <= right[r]) {
-                arr[i++] = left[l++];
-            } else {
-                arr[i++] = right[r++];
-            }
-        }
-
-        while (l < left.length) {
-            arr[i++] = left[l++];
-        }
-        
-        while (r < right.length) {
-            arr[i++] = right[r++];
-        }
-
+       	
+	while (true){ 
+		if (l < left.length && r < right.length) {
+		    if (left[l] <= right[r]) {
+			arr[i++] = left[l++];
+		    } else {
+			arr[i++] = right[r++];
+		    }
+		}
+		else if (l < left.length)
+		    arr[i++] = left[l++];	
+		else if (r < right.length)
+		    arr[i++] = right[r++];
+		else
+		    break;
+	}
         return arr;
     }
 
     public static void main(String[] args) {
         int[] arr = {5,9,1,3,4,6,6,3,2};
-        System.out.println(Arrays.toString(sort(arr)));
+        System.out.println(Arrays.toString(my_mergesort(arr)));
     }
 }
--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ rm -f a.out && gcc mergesort.c && ./a.out
 python3 mergesort.py
 rm -f mergesort && ghc mergesort.hs && ./mergesort
 rm -f mergesort && rustc mergesort.rs && ./mergesort
-rm -f Mergesort && javac Mergesort.java && java Mergesort
+rm -f mergesort && javac mergesort.java && java mergesort
 rhino mergesort.js
 php mergesort.php
