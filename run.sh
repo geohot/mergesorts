32012654bc230244c8187cc084b4a5073a98cdad
Adrien RICHARD <richardadrien0@gmail.com>
2020-07-18 18:18:41 +0200

    uodated files

--- run.sh
+++ run.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
-echo "Running the C version"
+echo "\n Running the C version"
 rm -f a.out && gcc mergesort.c && ./a.out
 
-echo "Running the Python version"
+echo "\n Running the Python version"
 python3 mergesort.py
 
-echo "Running the haskell version"
+echo "\n Running the haskell version"
 rm -f mergesort && ghc mergesort.hs && ./mergesort
 
-echo "Running the Rust version"
+echo "\n Running the Rust version"
 rm -f mergesort && rustc mergesort.rs && ./mergesort
 
-echo "Running the Java version"
+echo "\n Running the Java version"
 rm -f mergesort && javac mergesort.java && java mergesort
 
-echo "Running the Javascript version"
+echo "\n Running the Javascript version"
 node mergesort.js
 
-echo "Running the PHP version"
+echo "\n Running the PHP version"
 php mergesort.php
 
-echo "Running the Scala version"
+echo "\n Running the Scala version"
 scala mergesort.scala 
 
-echo "Running the C++ version"
+echo "\n Running the C++ version"
 rm -f mergesort && g++ -std=c++17 mergesort.cpp && ./a.out
 
-echo "Running the Julia version"
+echo "\n Running the Julia version"
 julia mergesort.jl
 
-echo "Running the Perl version"
+echo "\n Running the Perl version"
 perl mergesort.pl
 
-echo "Running the Go version"
+echo "\n Running the Go version"
 rm -f mergesort && go build mergesort.go && ./mergesort
 
-echo "Running the OCaml version"
+echo "\n Running the OCaml version"
 rm -f mergesort && ocamlc mergesort.ml -o mergesort && ./mergesort
 
-echo "Running the Bash version"
+echo "\n Running the Bash version"
 bash mergesort.sh
 
-echo "Running the C# version"
+echo "\n Running the C# version"
 csc mergesort.cs && mergesort
 
-echo "Running the Kotlin version"
+echo "\n Running the Kotlin version"
 kotlin mergesort.kt -include-runtime -d mergesort.jar && java -jar mergesort
 
-echo "Running the Prolog version"
+echo "\n Running the Prolog version"
 rm -f mergesort && swipl -g main --stand_alone=true -o mergesort -c mergesort.pro && ./mergesort
 
-echo "Running the J version"
+echo "\n Running the J version"
 jconsole mergesort.ijs
 
-echo "Running the Scheme version"
+echo "\n Running the Scheme version"
 scheme --script mergesort.ss
 
-echo "Running the Ruby version"
+echo "\n Running the Ruby version"
 ruby mergesort.rb
