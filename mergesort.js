866c7d30eaa845ce9cd0b1438c92bc186f3315c0
bozidardj <me.bozidar@gmail.com>
2020-07-19 22:36:58 +0200

    Update mergesort.js
    Oh, you fast JS :)

--- mergesort.js
+++ mergesort.js
@@ -1,49 +1,6 @@
-function mergesort(a){
-  
-  if (a.length == 1){
-    return a;
-  }
-  else if (a.length == 2){
-    if (a[0] > a[1]){
-      return [a[1], a[0]];
-    }
-    else{
-      return a;
-    }
-  }
-
-  var p = parseInt(a.length/2);
-  var m1 = mergesort(a.slice(0, p));
-  var m2 = mergesort(a.slice(p));
-
-  var ret = [];
-
-  while(1){
-    if(m1.length > 0 && m2.length > 0){
-      if(m1[0] <= m2[0]){
-        ret.push(m1[0]);
-        m1 = m1.slice(1);
-      }
-      else{
-        ret.push(m2[0]);
-        m2 = m2.slice(1);
-      }
-    }
-    else if(m1.length > 0){
-      ret = ret.concat(m1);
-      m1 = [];
-    }
-    else if (m2.length > 0){
-      ret = ret.concat(m2);
-      m2 = [];
-    }
-    else{
-      break;
-    }
-  }
-
-  return ret;
+var mergesort = a => a.length < 2 ? a: merge(mergesort(a.splice(0, a.length/2)),mergesort(a))
+var merge = (l,r,a=[]) => {
+  while(l.length && r.length) l[0]<r[0] ? a.push(l.shift()) : a.push(r.shift())
+  return [...a,...l,...r]
 }
-
-a = [5,9,1,3,4,6,6,3,2];
-console.log(mergesort(a))
+console.log(mergesort([5,9,1,3,4,6,6,3,2]))
