a2c311f40f09d48c2b437d537eb5c777d2f687bb
Chris Elbring <chris.elbring@gmail.com>
2020-07-20 00:14:02 -0500

    functional programming

--- mergesort.fsx
+++ mergesort.fsx
@@ -1,5 +1,16 @@
 //f sharp merge sort
 
+let rec merge list1 list2 =
+    match (list1, list2) with
+        | (_, []) -> list1
+        | ([], _) -> list2
+        | (_, _) ->
+            if list1.Head <= list2.Head then
+                list1.Head :: merge list1.Tail list2
+            else
+                list2.Head :: merge list2.Tail list1
+
+
 let rec mergesort list =
     match list with
         | [] -> []
@@ -7,3 +18,5 @@ let rec mergesort list =
         | _ -> let (subList1, subList2) = List.splitAt (List.length list / 2) list
                 in merge (mergesort subList1) (mergesort subList2)
 
+let unsorted = [5;9;1;3;4;6;6;3;2]
+printfn "%A" (mergesort unsorted)
