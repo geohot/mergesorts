6d1c130ddc9f51ef1d7020cf86a1c80906c75f68
Chris Elbring <chris.elbring@gmail.com>
2020-07-20 14:26:42 -0500

    R

--- run.sh
+++ run.sh
@@ -62,7 +62,7 @@ printf "\nRunning the Ruby version : \n"
 ruby mergesort.rb
 
 printf "\nRunning the R version : \n"
-which Rscript
+which R
 /usr/bin/Rscript mergesort.r 
 
 printf "\nRunning the Elixir version : \n"
