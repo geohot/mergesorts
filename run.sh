84d6cf3ffe3a7344f99c0ff96b72474ffa4b7775
jitwit <jrn@bluefarm.ca>
2020-07-20 13:38:47 -0400

    fix scala?

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ printf "\nRunning the PHP version : \n"
 php mergesort.php
 
 printf "\nRunning the Scala version : \n"
-scala mergesort.scala 
+scala -nc mergesort.scala
 
 printf "\nRunning the C++ version : \n"
 g++ -std=c++17 mergesort.cpp && ./a.out && rm -f a.out
