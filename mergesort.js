e29acd531b364c3990c8437a86c1b7e3cd1e13b7
bozidardj <me.bozidar@gmail.com>
2020-07-20 08:12:14 +0200

    Update mergesort.js

--- mergesort.js
+++ mergesort.js
@@ -1,6 +1,2 @@
-var mergesort = a => a.length < 2 ? a: merge(mergesort(a.splice(0, a.length/2)),mergesort(a))
-var merge = (l,r,a=[]) => {
-  while(l.length && r.length) l[0]<r[0] ? a.push(l.shift()) : a.push(r.shift())
-  return [...a,...l,...r]
-}
-console.log(mergesort([5,9,1,3,4,6,6,3,2]))
+var mergesort=a=>a.length<2?a:merge(mergesort(a.splice(0,a.length/2)),mergesort(a)),merge=(l,r,a=[])=>{for(;l.length&&r.length;)l[0]<r[0]?a.push(l.shift()):a.push(r.shift());return[...a,...l,...r]};
+console.log(mergesort([5,9,1,3,4,6,6,3,2]));
