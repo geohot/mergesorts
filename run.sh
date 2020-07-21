4cc5e584722646adeb075b9d30b0768df07f7ce5
Cool-fire <upendrareddy2511@gmail.com>
2020-07-21 16:25:04 +0530

    may the force be with you

--- run.sh
+++ run.sh
@@ -91,6 +91,9 @@ dotnet fsi mergesort.fsx
 printf "\nRunning the ATS version : \n"
 myatscc mergesort.dats && ./mergesort_dats
 
+printf "\nRunning the D Lang version : \n"
+rdmd mergesort.d
+
 end=`date +%s`
 runtime=$((end-start))
 echo "$runtime"
