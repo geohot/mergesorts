5c59cb7a11345af8f77913e22779e18e39e90a8b
Imaniac230 <horelican.t@gmail.com>
2020-07-24 21:08:14 +0300

    run.sh timings simplified

--- run.sh
+++ run.sh
@@ -2,166 +2,91 @@
 start=`date +%s`
 
 
-declare -a command_storage
-
 elapsed_time() {
 	if [ "$1" -ge "$2" ]; then elapsed_ns=$(("$1"-"$2")); else elapsed_ns=$(("$2"-"$1")); fi
 	echo "Time elapsed : $(($elapsed_ns/1000000000)) s $(($elapsed_ns/1000000 - ($elapsed_ns/1000000000)*1000)) ms"
 }
 
-run_all_commands() {
+run_lang() {
 	time_format=+%s%N
-	local -n commands=$1
-	for command in "${commands[@]}"
-	do
-		language_name=$(echo $command | cut -d '_' -f 2)
-		printf "\nRunning the $language_name version : \n"
-		version_start=`date $time_format`
-		${command}
-		version_end=`date $time_format`
-		if [ "$language_name" = "Ruby" ]; then printf "\n"; fi
-		elapsed_time "$version_start" "$version_end"
-	done
+	if [ "$1" = "" ]; then language="UNSPECIFIED"; else language="$1"; fi
+	printf "\nRunning the $language version : \n"
+	version_start=`date $time_format`
+	eval " $2 "
+	version_end=`date $time_format`
+	elapsed_time "$version_start" "$version_end"
 }
 
-run_C() {
-	rm -f a.out && gcc mergesort.c && ./a.out && rm -f a.out
-}; command_storage+=(run_C)
 
-run_Python() {
-	python3 mergesort.py
-}; command_storage+=(run_Python)
+#run_lang "language name" "command to execute"
 
-run_Haskell() {
-	rm -f mergesort && ghc mergesort.hs && ./mergesort
-}; command_storage+=(run_Haskell)
+run_lang C "rm -f a.out && gcc mergesort.c && ./a.out && rm -f a.out"
 
-run_Rust() {
-	rm -f mergesort && rustc mergesort.rs && ./mergesort
-}; command_storage+=(run_Rust)
+run_lang Python "python3 mergesort.py"
 
-run_Java() {
-	rm -f mergesort && javac mergesort.java && java mergesort
-}; command_storage+=(run_Java)
+run_lang Haskell "rm -f mergesort && ghc mergesort.hs && ./mergesort"
 
-run_Javascript() {
-	node mergesort.js
-}; command_storage+=(run_Javascript)
+run_lang Rust "rm -f mergesort && rustc mergesort.rs && ./mergesort"
 
-run_PHP() {
-	php mergesort.php
-}; command_storage+=(run_PHP)
+run_lang Java "rm -f mergesort && javac mergesort.java && java mergesort"
 
-run_Scala() {
-	scala -nc mergesort.scala
-}; command_storage+=(run_Scala)
+run_lang Javascript "node mergesort.js"
 
-run_C++() {
-	g++ -std=c++17 mergesort.cpp && ./a.out && rm -f a.out
-}; command_storage+=(run_C++)
+run_lang PHP "php mergesort.php"
 
-run_Julia() {
-	julia mergesort.jl
-}; command_storage+=(run_Julia)
+run_lang Scala "scala -nc mergesort.scala"
 
-run_Perl() {
-	perl mergesort.pl
-}; command_storage+=(run_Perl)
+run_lang C++ "g++ -std=c++17 mergesort.cpp && ./a.out && rm -f a.out"
 
-run_Go() {
-	rm -f mergesort && go build mergesort.go && ./mergesort
-}; command_storage+=(run_Go)
+run_lang Julia "julia mergesort.jl"
 
-run_OCaml() {
-	rm -f mergesort && ocamlc mergesort.ml -o mergesort && ./mergesort
-}; command_storage+=(run_OCaml)
+run_lang Perl "perl mergesort.pl"
 
-run_Bash() {
-	bash mergesort.sh
-}; command_storage+=(run_Bash)
+run_lang Go "rm -f mergesort && go build mergesort.go && ./mergesort"
 
-run_C#() {
-	mcs -out:mergesort.exe mergesort.cs && mono mergesort.exe
-}; command_storage+=(run_C#)
+run_lang OCaml "rm -f mergesort && ocamlc mergesort.ml -o mergesort && ./mergesort"
 
-run_Kotlin() {
-	kotlinc mergesort.kt -include-runtime -d mergesort.jar && java -jar mergesort.jar
-}; command_storage+=(run_Kotlin)
+run_lang Bash "bash mergesort.sh"
 
-run_Prolog() {
-	rm -f mergesort && swipl -g main --stand_alone=true -o mergesort -c mergesort.pro && ./mergesort
-}; command_storage+=(run_Prolog)
+run_lang C# "mcs -out:mergesort.exe mergesort.cs && mono mergesort.exe"
 
-run_J() {
-	ijconsole mergesort.ijs
-}; command_storage+=(run_J)
+run_lang Kotlin "kotlinc mergesort.kt -include-runtime -d mergesort.jar && java -jar mergesort.jar"
 
-run_Scheme() {
-	scheme --script mergesort.ss
-}; command_storage+=(run_Scheme)
+run_lang Prolog "rm -f mergesort && swipl -g main --stand_alone=true -o mergesort -c mergesort.pro && ./mergesort"
 
-run_Ruby() {
-	ruby mergesort.rb
-}; command_storage+=(run_Ruby)
+run_lang J "ijconsole mergesort.ijs"
 
-run_R() {
-	Rscript mergesort.r
-}; command_storage+=(run_R)
+run_lang Scheme "scheme --script mergesort.ss"
 
-run_Elixir() {
-	elixir mergesort.exs
-}; command_storage+=(run_Elixir)
+run_lang Ruby "ruby mergesort.rb"
 
-run_Dart() {
-	dart mergesort.dart
-}; command_storage+=(run_Dart)
+run_lang R "Rscript mergesort.r"
 
-run_Coq() {
-	coqc mergesort.v
-}; command_storage+=(run_Coq)
+run_lang Elixir "elixir mergesort.exs"
 
-run_LUA() {
-	lua5.3 mergesort.lua
-}; command_storage+=(run_LUA)
+run_lang Dart "dart mergesort.dart"
 
-run_TypeScript() {
-	tsc mergesort.ts --outDir out && node mergesort.js
-}; command_storage+=(run_TypeScript)
+run_lang Coq "coqc mergesort.v"
 
-run_Coffeescript() {
-	coffee mergesort.coffee
-}; command_storage+=(run_Coffeescript)
+run_lang LUA "lua5.3 mergesort.lua"
 
-run_Swift() {
-	swift mergesort.swift
-}; command_storage+=(run_Swift)
+run_lang TypeScript "tsc mergesort.ts --outDir out && node mergesort.js"
 
-run_F#() {
-	dotnet fsi mergesort.fsx
-}; command_storage+=(run_F#)
+run_lang Coffeescript "coffee mergesort.coffee"
 
-run_ATS() {
-	myatscc mergesort.dats && ./mergesort_dats
-}; command_storage+=(run_ATS)
+run_lang Swift "swift mergesort.swift"
 
-run_D() {
-	rdmd mergesort.d
-}; command_storage+=(run_D)
+run_lang F# "dotnet fsi mergesort.fsx"
 
-run_Brainfuck() {
-	bf mergesort.b
-}; command_storage+=(run_Brainfuck)
+run_lang ATS "myatscc mergesort.dats && ./mergesort_dats"
 
-run_TCL() {
-	tclsh mergesort.tcl
-}; command_storage+=(run_TCL)
+run_lang D "rdmd mergesort.d"
 
-run_Objective-C() {
-	clang -fobjc-arc -framework Foundation mergesort.m -o mergesort && ./mergesort
-}; command_storage+=(run_Objective-C)
+run_lang Brainfuck "bf mergesort.b"
 
-run_all_commands command_storage
+run_lang TCL "tclsh mergesort.tcl"
 
+run_lang Objective-C  "clang -fobjc-arc -framework Foundation mergesort.m -o mergesort && ./mergesort"
 
 end=`date +%s`
 runtime=$((end-start))
