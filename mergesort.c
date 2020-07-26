e9c0160ba2321e3d58314886402206374bff4b56
Ricardo Biehl Pasquali <pasqualirb@gmail.com>
2020-07-26 01:15:13 -0300

    Make mergesort.c 70% smaller in lines of code
    Now it is between Scheme and Haskell:
    
      15 mergesort.ss
      18 mergesort.c
      19 mergesort.hs

--- mergesort.c
+++ mergesort.c
@@ -1,60 +1,18 @@
-// let's code mergesort in c, can you do?
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-void my_mergesort(int *arr, int len) {
-  //printf("%p %d\n", arr, len);
-  if (len == 1) { return; }
-  if (len == 2) {
-    if (arr[0] > arr[1]) {
-      int t = arr[1];
-      arr[1] = arr[0];
-      arr[0] = t;
-    }
-  }
-
-  int p = len/2;
-  int *arr1 = arr;
-  int *arr2 = arr+p;
-
-  my_mergesort(arr1, p);
-  my_mergesort(arr2, len-p);
-
-  int *t = malloc(sizeof(int)*len);
-  int *rt = t;
-  while (1) {
-    if (arr1 < arr+p && arr2 < arr+len) {
-      if (*arr1 <= *arr2) {
-        *t = *arr1;
-        arr1++;
-      } else {
-        *t = *arr2;
-        arr2++;
-      }
-    } else if(arr1 < arr+p) {
-      *t = *arr1;
-      arr1++;
-    } else if(arr2 < arr+len) {
-      *t = *arr2;
-      arr2++;
-    } else {
-      break;
-    }
-    t++;
-  }
-
-  memcpy(arr, rt, sizeof(int)*len);
-  free(rt);
+int* merge(int *a, int *b, int s) {
+	int *A=a+(s/2), *B=b+(s-s/2), v[s], *V=v+s, *i=v;
+	while (i<V) *i++ = (*a <= *b && a!=A || b==B) ? *a++ : *b++;
+	while (s--) *--b = *--i; // copy in reverse to original array
+	return b;
 }
 
-int main() {
-  int a[] = {5,9,1,3,4,6,6,3,2};
-  int len = sizeof(a)/sizeof(int);
-  my_mergesort(a, sizeof(a)/sizeof(int));
-  for (int i = 0; i < len; i++) {
-    printf("%d ", a[i]);
-  }
-  printf("\n");
+int* mergesort(int *a, int s) {
+	if (s == 1) return a;
+	return merge(mergesort(a, s/2), mergesort(a+s/2, s-s/2), s);
 }
 
+void main() {
+	int a[] = {5,9,1,3,4,6,6,3,2}; mergesort(a, 10);
+	int i=0; while (i < 9) printf("%d ", a[i++]); printf("\n");
+}
