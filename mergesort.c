bf248f89e68fda215d1a2de7d1346896a4f06595
Ricardo Biehl Pasquali <pasqualirb@gmail.com>
2020-07-26 01:15:13 -0300

    Reduce lines of mergesort.c by 50% while keeping readability

--- mergesort.c
+++ mergesort.c
@@ -1,60 +1,30 @@
-// let's code mergesort in c, can you do?
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-void my_mergesort(int *arr, int len) {
-  //printf("%p %d\n", arr, len);
-  if (len == 1) { return; }
-  if (len == 2) {
-    if (arr[0] > arr[1]) {
-      int t = arr[1];
-      arr[1] = arr[0];
-      arr[0] = t;
-    }
-  }
+int* merge(int *A, int *B, int len) {
+	int *array = A, *endA = A+(len/2), *endB = A+len;
+	int tmp[len];
+	for (int i = 0; i < len; i++)
+		if ((*A <= *B && A != endA) || B == endB)
+			tmp[i] = *A++;
+		else
+			tmp[i] = *B++;
+	for (int i = 0; i < len; i++)
+		array[i] = tmp[i];
+	return array;
+}
 
-  int p = len/2;
-  int *arr1 = arr;
-  int *arr2 = arr+p;
-
-  my_mergesort(arr1, p);
-  my_mergesort(arr2, len-p);
-
-  int *t = malloc(sizeof(int)*len);
-  int *rt = t;
-  while (1) {
-    if (arr1 < arr+p && arr2 < arr+len) {
-      if (*arr1 <= *arr2) {
-        *t = *arr1;
-        arr1++;
-      } else {
-        *t = *arr2;
-        arr2++;
-      }
-    } else if(arr1 < arr+p) {
-      *t = *arr1;
-      arr1++;
-    } else if(arr2 < arr+len) {
-      *t = *arr2;
-      arr2++;
-    } else {
-      break;
-    }
-    t++;
-  }
-
-  memcpy(arr, rt, sizeof(int)*len);
-  free(rt);
+int* mergesort(int *A, int len) {
+	if (len == 1)
+		return A;
+	return merge(mergesort(A, len/2), mergesort(A+len/2, len-len/2), len);
 }
 
 int main() {
-  int a[] = {5,9,1,3,4,6,6,3,2};
-  int len = sizeof(a)/sizeof(int);
-  my_mergesort(a, sizeof(a)/sizeof(int));
-  for (int i = 0; i < len; i++) {
-    printf("%d ", a[i]);
-  }
-  printf("\n");
+	int A[] = {5,9,1,3,4,6,6,3,2};
+	int len = sizeof(A)/sizeof(int);
+	mergesort(A, len);
+	for (int i = 0; i < len; i++)
+		printf("%d ", A[i]);
+	printf("\n");
+	return 0;
 }
-
