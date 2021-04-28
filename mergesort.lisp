af566e63d7cccdbf8e8e4889bea1db2d3c3a102f
Georgy Trush <lkitjdp@gmail.com>
2021-04-28 21:06:05 +0300

    Add Common Lisp :yin_yang:

---
+++ mergesort.lisp
@@ -0,0 +1,15 @@
+(defun mergesort (list)
+    (labels ((merge (f s)
+            (cond
+                ((null f) s)
+                ((null s) f)
+                ((< (first f) (first s)) (list* (first f) (merge (rest f) s)))
+                ((> (first f) (first s)) (list* (first s) (merge f (rest s))))
+                ((= (first f) (first s)) (list* (first f)
+                                                (first s) (merge (rest f) (rest s)))))))
+        (let ((len (list-length list)))
+            (if (<= len 1) list
+                (merge (mergesort (subseq list 0 (ceiling len 2)))
+                       (mergesort (subseq list (ceiling len 2))))))))
+
+(write(mergesort(list 5 9 1 3 4 6 6 3 2)))
