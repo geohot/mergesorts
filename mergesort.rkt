5ef969a7fc64fb8aeba094abd1b0c896f83b4bb5
Jacky Lee <jla524@sfu.ca>
2021-11-20 14:45:17 -0800

    Mergesort in Racket

---
+++ mergesort.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (merge-lists xs ys)
+  (cond
+    [(null? xs) ys]
+    ;if xs is empty, return ys
+    [(null? ys) xs]
+    ;if ys is empty, return xs
+    [(< (car xs) (car ys))
+    ;if the head of list "xs" is bigger than head of list "ys"
+     (cons (car xs) (merge-lists (cdr xs) ys))]
+     ;cons head xs to (recurse)
+    [#t ;I use true as an else cond here - possibly bad style?
+     (cons (car ys) (merge-lists xs (cdr ys)))]))
+     ;cons head ys to (recurse)
+
+(define (merge-sort xs)
+  (cond
+    [(or (null? xs) (null? (cdr xs))) xs]
+    [(null? (cddr xs))
+     (merge-lists (list (car xs)) (cdr xs))]
+    [#t
+     (let ([x (ceiling (/ (length xs) 2))])
+       (merge-lists (merge-sort (take xs x))
+                    (merge-sort (drop xs x))))]))
+
+(merge-sort (list 5 9 1 3 4 6 6 3 2))
--- run.sh
+++ run.sh
@@ -112,6 +112,9 @@ rm -f mergesort.ali mergesort.o mergesort && gnatmake mergesort.adb && ./mergeso
 printf "\nRunning the Pascal version : \n"
 rm -f mergesort mergesort.o && fpc mergesort.pas &> /dev/null && ./mergesort && rm -f mergesort.o mergesort
 
+printf "\nRunning the Racket version : \n"
+racket mergesort.rkt
+
 end=`date +%s`
 runtime=$((end-start))
 printf "\n"
